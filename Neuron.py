ac0f634d4f523a60a4b10c7549795514f13e25bd
Amrit Das <amritdas8015@gmail.com>
2024-06-06 22:24:14 -0400

    6/6

--- Neuron.py
+++ Neuron.py
@@ -1,56 +1,33 @@
 import streamlit as st
 from neuron import h
-from neuron.units import ms, mV, µm
+import numpy as np
 import matplotlib.pyplot as plt
 
-# Streamlit interface
 st.title('NEURON Simulation with Streamlit')
-voltage = st.slider("Choose the voltage with which to run the simulation", min_value=-80, max_value=-55, value=-65)
-st.write = print(voltage)
 
+# Initialize NEURON
+soma = h.Section(name='soma')
+soma.L = 20
+soma.diam = 20
 
-# Function to run the NEURON simulation
-def run_simulation(voltage):
-    # Load standard run library
-    h.load_file("stdrun.hoc")
+h.dt = 0.025
+h.tstop = 100
 
-    # Create soma section
-    soma = h.Section(name='soma')
-    soma.L = 20  # microns
-    soma.diam = 20  # microns
+v_soma = h.Vector()
+v_soma.record(soma(0.5)._ref_v)
 
-    # Simulation parameters
-    h.dt = 0.025  # time step (ms)
-    h.tstop = 100  # simulation stop time (ms)
+# Run the simulation
+h.finitialize(-65)
+h.continuerun(h.tstop)
 
-    # Record membrane potential at the center of the soma
-    v_soma = h.Vector().record(soma(0.5)._ref_v)
-    t = h.Vector().record(h._ref_t)
+# Retrieve and plot the data
+v_soma_array = np.array(v_soma)
 
-    # Define a simple mechanism (e.g., Hodgkin-Huxley)
-    soma.insert('hh')
+fig, ax = plt.subplots()
+ax.plot(np.arange(0, h.tstop + h.dt, h.dt), v_soma_array)
+ax.set_xlabel('Time (ms)')
+ax.set_ylabel('Membrane Potential (mV)')
+ax.set_title('Membrane Potential vs. Time')
 
-    # Run the simulation
-    h.finitialize(voltage * mV)
-    h.run()
-
-    return t, v_soma
-
-# Function to plot the results
-def plot_results(t, v_soma):
-    plt.figure(figsize=(8, 4))
-    plt.plot(t, v_soma)
-    plt.xlabel('Time (ms)')
-    plt.ylabel('Membrane Potential (mV)')
-    plt.title('Membrane Potential vs. Time in Soma')
-    st.pyplot(plt)
-
-# Slider for selecting the initial voltage
-voltage = st.slider("Choose the voltage with which to run the simulation", min_value=-80, max_value=-55, value=-65)
-
-# Run the simulation when the button is pressed
-if st.button('Run Simulation'):
-    st.write('Running simulation...')
-    t, v_soma = run_simulation(voltage)
-    st.write('Simulation completed.')
-    plot_results(t, v_soma)
+# Display the plot in Streamlit
+st.pyplot(fig)
